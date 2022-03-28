559493c22f65b094300a0c43c5e9e05a6ddf0894
viji20182019 <101647643+viji20182019@users.noreply.github.com>
2022-03-28 17:47:09 +0530

    Update providers.tf

--- VM create/providers.tf
+++ VM create/providers.tf
@@ -12,3 +12,9 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+provider "azuread" {
+  client_id     = "fda1394b-3c13-4c25-8930-716ab979ddc8"
+  client_secret = E537Q~X6cmzK1o6xsr8LaL.OjsnmcXBvBg1oq
+  tenant_id     = "4a1c1070-2a5b-4bdf-b773-6055c2c740a4"
+}
